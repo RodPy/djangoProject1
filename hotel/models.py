ac89803e076f588ecd08103a09c0adfc17d7e8bd
RodPy <rodneyrrspy@gmail.com>
2021-11-09 02:18:03 -0300

    fix models

--- hotel/models.py
+++ hotel/models.py
@@ -7,12 +7,12 @@ from django.db import models
 class Departamento(models.Model):
    # dueno= models.CharField(max_length=50)
     dueno= models.ForeignKey(settings.AUTH_USER_MODEL,null=True, blank=False, on_delete=models.CASCADE)
-    cantHab= models.IntegerField(10)
+    cantHab= models.IntegerField()
 
 class Dispositivo(models.Model):
     nombre= models.CharField(max_length=50)
     tipo=models.CharField(max_length=20)
-    pinGpio=models.PositiveIntegerField(2)
+    pinGpio=models.PositiveIntegerField()
 
 class Piso(models.Model):
     nro = models.IntegerField(2)
