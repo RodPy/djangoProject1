1d1574858f287010b6a9ae19ab33610dc05bf566
RodPy <rodneyrrspy@gmail.com>
2021-11-09 02:06:41 -0300

    add los modelos a admin.py , prueba del uso del User en el modelo

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from hotel.models import *
+
+admin.site.register(Departamento)
+admin.site.register(Dispositivo)
+admin.site.register(Piso)
+admin.site.register(habitacion)
+
--- hotel/models.py
+++ hotel/models.py
@@ -1,18 +1,22 @@
+from django.conf import settings
+
 from django.db import models
 
 # Create your models here.
 
 class Departamento(models.Model):
-    dueno= models.CharField(max_length=50)
+   # dueno= models.CharField(max_length=50)
+    dueno= models.ForeignKey(settings.AUTH_USER_MODEL,null=True, blank=False, on_delete=models.CASCADE)
     cantHab= models.IntegerField(10)
 
 class Dispositivo(models.Model):
     nombre= models.CharField(max_length=50)
     tipo=models.CharField(max_length=20)
-  #  pinGpio=models.IntegerField(10)
+    pinGpio=models.PositiveIntegerField(2)
 
 class Piso(models.Model):
     nro = models.IntegerField(2)
+    cantDept=models.PositiveIntegerField(2)
 
 class habitacion(models.Model):
     dispositivo=models.ForeignKey('Dispositivo', null= True, blank=False, on_delete=models.CASCADE)
